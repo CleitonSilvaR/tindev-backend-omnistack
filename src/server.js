1320bfa6699348b10c9e33a00af5db5e57f9e873
Cleiton Silva <cleiton_.98@hotmail.com>
2019-08-20 20:13:48 -0300

    1/2 part back finished

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
+const connections = require('../credentials/connections.json');
+
 
 const server = express();
 
+mongoose.connect(
+  connections.connectionMongoDB,
+  { useNewUrlParser: true }
+);
+
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
